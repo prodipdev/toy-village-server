b84d86f54bd2ea5f11f451b138f07b7254fcbe1e
prodipdev <prodipink@gmail.com>
2023-05-21 07:45:47 +0600

    Fixing sorting issue and more

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,15 +26,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toysCollection = client.db("toyVillage").collection("allToys");
 
     // Get all toys from the database
     app.get("/toys", async (req, res) => {
-      const cursor = toysCollection.find();
+      const cursor = toysCollection.find().sort({ createdAt: -1 });
       const result = await cursor.toArray();
-      res.send(result);
+      res.json(result);
     });
 
     // Search for toys by name
@@ -44,23 +44,38 @@ async function run() {
         .find({
           name: { $regex: text, $options: "i" },
         })
+        .sort({ createdAt: -1 })
         .toArray();
-      res.send(result);
+      res.json(result);
     });
 
-    // Get specific user toy collection
+    // Get specific user toy collection which is depended on sorting
     app.get("/myToys/:email", async (req, res) => {
+      const email = req.params.email;
+      const sort = req.query.sort;
+
+      let sortQuery = {};
+      if (sort === "highest") {
+        sortQuery = { price: -1 };
+      } else if (sort === "lowest") {
+        sortQuery = { price: 1 };
+      }
+
       const result = await toysCollection
-        .find({ sellerMail: req.params.email })
+        .find({ sellerMail: email })
+        .sort(sortQuery)
         .toArray();
-      res.send(result);
+
+      res.json(result);
     });
+
     // Get toy collection for specific category
     app.get("/toyList/:category", async (req, res) => {
       const result = await toysCollection
         .find({ subCategory: req.params.category })
+        .sort({ createdAt: -1 })
         .toArray();
-      res.send(result);
+      res.json(result);
     });
 
     //   Get Single Toy Data by id
@@ -68,22 +83,23 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.findOne(query);
-      res.send(result);
+      res.json(result);
     });
 
     // Add a new toy to the database
     app.post("/addToy", async (req, res) => {
       const toy = req.body;
+      toy.createdAt = new Date();
+      toy.price = Number(toy.price); // Convert price to a number
       const result = await toysCollection.insertOne(toy);
-      res.send(result);
+      res.json(result);
     });
 
     // Update Toy Details
-    app.patch("/toyDetails/:id", async (req, res) => {
+    app.patch("/toyUpdate/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedToy = req.body;
-      console.log(updatedToy);
       const updateDoc = {
         $set: {
           name: updatedToy.name,
@@ -100,7 +116,7 @@ async function run() {
       };
 
       const result = await toysCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      res.json(result);
     });
 
     // Delete Toy from database
@@ -108,14 +124,14 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.deleteOne(query);
-      res.send(result);
+      res.json(result);
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
